d3063190bd55aaaabd79acce58beb2846d370c3e
Matthew McDermott <mmcdermott1214@gmail.com>
2023-04-17 17:29:46 -0600

    working start screen and in progress username interface

--- src/main/java/com/example/battleboggle/HelloApplication.java
+++ src/main/java/com/example/battleboggle/HelloApplication.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startScene.fxml"));
+        Scene scene1 = new Scene(fxmlLoader.load());
+        stage.setTitle("Boggle Battle");
+        stage.setScene(scene1);
         stage.show();
     }
 
