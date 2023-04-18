7760e51317f0e46f31df74d38bf43c3d8b3ed742
Matthew McDermott <mmcdermott1214@gmail.com>
2023-04-18 14:55:29 -0600

    rest of the views and controllers added

--- src/main/java/com/example/battleboggle/LeaderboardController.java
+++ src/main/java/com/example/battleboggle/LeaderboardController.java
@@ -1,34 +1,31 @@
 package com.example.battleboggle;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class GameboardController {
+public class LeaderboardController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    @FXML
-    private Text timer;
 
     public void switchScene(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("score-view.fxml"));
+        root = FXMLLoader.load(getClass().getResource("gameboard-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    public void closeButton(ActionEvent event) {
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
+
 }
--- src/main/java/com/example/battleboggle/ScoreController.java
+++ src/main/java/com/example/battleboggle/ScoreController.java
@@ -1,31 +1,22 @@
 package com.example.battleboggle;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class GameboardController {
+public class ScoreController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    @FXML
-    private Text timer;
 
     public void switchScene(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("score-view.fxml"));
+        root = FXMLLoader.load(getClass().getResource("leaderboard-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
